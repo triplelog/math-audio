55270a7b1afc56324d0979243ee658a449548597
triplelog <robmwilc@gmail.com>
2022-12-09 15:53:49 -0500

    start math to audio conversion

--- mathToAudio.js
+++ mathToAudio.js
@@ -1,7 +1,90 @@
 function mathToAudio(){
 	var input = document.getElementById('textEntry').value;
 	var cleaned = clean(input);
-	console.log(cleaned);
 	var postfixed = postfixify(cleaned);
 	console.log(postfixed);
+	var audio = toAudio(postfixed);
+	//TODO: remove consecutive spaces
+	console.log(audio);
 }
+
+
+function toAudio(postfixList) {
+
+    var stack = [];
+    var stackIndex = 0;
+    for (var i = 0; i < postfixList.length; i++) {
+        var c = postfixList[i];
+        if (prec[c]) {
+            var fn = toAudioOp[c];
+            if (fn) {
+                var result = fn(stack, stackIndex, c);
+                stack = result[0];
+                stackIndex = result[1];
+            } else {
+                var lastPrec = prec[stack[stackIndex - 1].op];
+                stack[stackIndex - 1].exp = c + " of open parentheses " + stack[stackIndex - 1].exp + " close parentheses ";
+                stack[stackIndex - 1].op = c;
+            }
+        } else if (c[0] == "\\") {
+            var fn = toAudioOp[c.substring(1)];
+            if (fn) {
+                var result = fn(stack, stackIndex, c);
+                stack = result[0];
+                stackIndex = result[1];
+            } else {
+                if (c.length > 1 && c[1] == ":") {
+                    stack[stackIndex - 1].exp = " " + c.replace("\\:", "") + " of open parentheses " + stack[stackIndex - 1].exp + " close parentheses ";
+                    stack[stackIndex - 1].op = '#';
+                } else {
+                    if (toAudioExp[c]) {
+                        c = toAudioExp[c];
+                    }
+                    stack[stackIndex] = {
+                        "exp": c,
+                        "op": '#'
+                    };
+                    stackIndex++;
+                }
+
+            }
+        } else {
+            if (toAudioExp[c]) {
+                c = toAudioExp[c];
+            } else if (isNumber(c)) {
+                c = toNumberAudio(c);
+            }
+            stack[stackIndex] = {
+                "exp": c,
+                "op": '#'
+            };
+            stackIndex++;
+        }
+    }
+    return stack[0].exp;
+}
+
+function toNumberAudio(input){
+	return " " + input + " ";
+}
+
+var toAudioOp = {};
+toAudioOp["~"] = function(stack, stackIndex, op){
+	stack[stackIndex-1].exp = " negative one times "+stack[stackIndex - 1].exp;
+	stack[stackIndex - 1].op = op;
+	return [stack, stackIndex];
+}
+toAudioOp["*"] = function(stack, stackIndex, op){
+	stack[stackIndex-2].exp = stack[stackIndex - 2].exp+" times "+stack[stackIndex - 1].exp;
+	stack[stackIndex - 2].op = op;
+	stackIndex--;
+	return [stack, stackIndex];
+}
+toAudioOp["+"] = function(stack, stackIndex, op){
+	stack[stackIndex-2].exp = stack[stackIndex - 2].exp+" plus "+stack[stackIndex - 1].exp;
+	stack[stackIndex - 2].op = op;
+	stackIndex--;
+	return [stack, stackIndex];
+}
+
+var toAudioExp = {};
